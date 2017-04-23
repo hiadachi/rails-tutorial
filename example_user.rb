f00ba3a847d7efd003130c35009b11002ed70c7d
hiadachi <b1504ha@aiit.ac.jp>
2017-04-23 03:19:37 +0000

    Finish Layout and routes

---
+++ example_user.rb
@@ -0,0 +1,23 @@
+class User
+  attr_accessor :first_name, :last_name, :email
+
+  def initialize(attributes = {})
+   @first_name = attributes[:first_name]
+   @last_name = attributes[:last_name]
+  # @name = attributes[:name]
+   @email= attributes[:email]
+  end
+  
+  def full_name
+    @full_name = @last_name + @first_name
+  end
+  
+  def alphabetical_name
+    @full_name = @last_name + ", " + @first_name
+  end
+  
+  
+  def formatted_email
+    "#{@full_name} <#{@email}>"
+  end
+end
